32f114c783c9fb7132a6fd645d2e7b53c8f2f3a5
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-04-12 17:42:12 -0500

    stages

--- packages/web/src/components/Contact/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const ContactComponent: React.FC = () => {
-	return <div></div>;
-};
--- packages/web/src/containers/Potfolio/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const PortfolioContainer: React.FC = () => {
-	return <div>asd</div>;
-};
